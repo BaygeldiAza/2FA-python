15bc017c5fa8eca8afadeb119e7a0254f3d119d7
Baygeldi <baygeldi0718@gmail.com>
2026-02-02 00:58:43 +0800

    bugs fixed

--- src/models.py
+++ src/models.py
@@ -1,19 +1,18 @@
 from sqlalchemy import create_engine, Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 
-from .db import engine
-
-
 Base = declarative_base()
 
 class User(Base):
     __tablename__ = "users"
     
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(255), index=True, nullable=False)  # Added length here
-    email = Column(String(255), unique=True, index=True, nullable=False)  # Specify length here too
-    hashed_password = Column(String(128))  # Specify length if needed
-    otp = Column(String(6), nullable=True)  # Adjusted length of OTP if necessary
-    otp_expires_at = Column(DateTime, nullable=True)
+    username = Column(String(255), index=True, nullable=False)  
+    email = Column(String(255), unique=True, index=True, nullable=False)  
+    hashed_password = Column(String(255)) 
+    otp = Column(String(6), nullable=False)  
+    otp_expires_at = Column(DateTime, nullable=False)
 
-Base.metadata.create_all(bind=engine)
+    def __repr__(self):
+        return f"<User(id='{self.id}', username='{self.username}', email='{self.email}')>"
+
