faadd073666f451123d4135ce07cd063c1cbb99f
Baygeldi <baygeldi0718@gmail.com>
2026-02-20 23:41:11 +0800

    Env added into config.py to generate refresh token logic

--- src/config.py
+++ src/config.py
@@ -43,5 +43,6 @@ class Settings:
     SECRET_KEY: str = os.getenv("SECRET_KEY")
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    REFRESH_TOKEN_EXPIRE_DAYS: int = 7
 
 settings = Settings()
