3d388c07a5b241dc2dd52446c4e1e7545fe491cd
Baygeldi <baygeldi0718@gmail.com>
2026-03-04 21:32:28 +0800

    Refactored, seperation concern

--- src/models.py
+++ src/models.py
@@ -4,48 +4,49 @@ from sqlalchemy.sql import func
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
 
-Base = declarative_base()
+BaseModel = declarative_base()
 
-class User(Base):
+class User(BaseModel):
     __tablename__ = "users"
-    
+
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(255), index=True, nullable=False)  
-    email = Column(String(255), unique=True, index=True, nullable=False)  
-    hashed_password = Column(String(255),nullable=False) 
+    username = Column(String(255), index=True, nullable=False)
+    email = Column(String(255), unique=True, index=True, nullable=False)
+    hashed_password = Column(String(255), nullable=True)
 
     refresh_tokens = relationship(
-        "Refresh-Token",
+        "RefreshToken",
         back_populates="user",
         cascade="all, delete-orphan"
     )
 
-    #OAuth fields
-    oauth_provider = Column(String(50), nullable=True)
+    oauth_provider = Column(String(255), nullable=True)
     oauth_id = Column(String(255), nullable=True)
     profile_picture = Column(String(500), nullable=True)
     is_verified = Column(Boolean, default=False)
 
-    #OTP fields
-    otp = Column(String(6), nullable=True)  
-    otp_expires_at = Column(DateTime, nullable=True)
+    otp = Column(String(6), nullable=True)
+    otp_expires_at = Column(DateTime, nullable=False)
     otp_attempts = Column(Integer, default=0)
     user_created_time = Column(DateTime, default=datetime.utcnow, server_default=func.now(), nullable=False)
+
     def __repr__(self):
         return f"<User(id='{self.id}', username='{self.username}', email='{self.email}')>"
 
-
-class Refresh_Token(Base):
+class RefreshToken(BaseModel):
     __tablename__ = "refresh_tokens"
 
-    id = Column(Integer,primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"),index=True, nullable=False)
-    token_hash = Column(String(255),nullable=False)
-    expires_at = Column(DateTime,nullable=False)
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), index=True, nullable=False)
+    token_hash = Column(String(255), nullable=False, unique=True, index=True)
+    expires_at = Column(DateTime, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow, server_default=func.now(), nullable=False)
-    revoked = Column(Boolean,default=False)
+    revoked = Column(Boolean, default=False)
 
     user = relationship(
         "User",
         back_populates="refresh_tokens"
     )
+
+    def __repr__(self):
+        return f"<RefreshToken(id='{self.id}', user_id='{self.user_id}', revoked='{self.revoked}')>"
